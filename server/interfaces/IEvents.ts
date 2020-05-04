f0e4f341c5c8da52e5e64b0e2f54aa320c734bfa
Mark Wissink <markcwissink@gmail.com>
2020-05-04 09:22:23 -0400

    interfaces

---
+++ server/interfaces/IEvents.ts
@@ -0,0 +1,48 @@
+export namespace Events {
+  export namespace Room {
+    export interface Create {
+      name: string;
+      theme: any;
+    };
+    export interface Update extends Create {};
+    export interface Join {
+      id: string;
+    }
+  }
+  export namespace Piano {
+    export interface NoteOn {
+      id: string;
+      color: string;
+      note: {
+        number: number,
+        velocity: number,
+      };
+    }
+    export interface NoteOff {
+      id: string;
+      note: {
+        number: number,
+      };
+    }
+    export interface ControlChange {
+      id: string;
+      control: {
+        number: number,
+        value: number,
+      };
+    }
+  }
+  export interface Chat {
+    id: string;
+    message: string;
+  }
+  export interface Permissions {
+    id: string;
+    permissions: any;
+  }
+  export interface Settings {
+    name: string;
+    color: string,
+  }
+
+}
