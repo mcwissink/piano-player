d90fe88ca332132eb7b8cadea4012b68c56866cc
Mark Wissink <markcwissink@gmail.com>
2020-11-02 22:05:50 -0500

    Svg piano roll

--- client/src/components/Piano.tsx
+++ client/src/components/Piano.tsx
@@ -26,6 +26,8 @@ class Piano extends React.PureComponent<IProps, IPianoState> {
   blackKeyMapping = [1, -1, 4, 6, -1, 9, 11];
   blackKeyMappingNoPadding = [1, 4, 6, 9, 11];
   keyboard: JSX.Element[] = [];
+  pianoRoll: Array<JSX.Element[]> = [];
+  pianoRollActive: any[] = [];
   constructor(props: IProps) {
     super(props);
     this.frameId = -1;
@@ -38,6 +40,9 @@ class Piano extends React.PureComponent<IProps, IPianoState> {
       midiFileDump: '',
     };
     this.drawPiano();
+    for (let i = 0; i < 88; i++) {
+      this.pianoRoll[i] = [];
+    }
     this.midi = new MidiController(
       this.props.socket,
       this.props.user.color,
@@ -111,26 +116,135 @@ class Piano extends React.PureComponent<IProps, IPianoState> {
     document.body.removeChild(link);
   }
 
+  heightAnimation = (relativeNote: number) => (e: any) => {
+    if (e) {
+      this.pianoRollActive[relativeNote] = e;
+      e.beginElement();
+    }
+  }
+
+  positionAnimation = (relativeNote: number) => (e: any) => {
+    if (e) {
+      e.addEventListener('endEvent', () => {
+        this.pianoRoll[relativeNote].shift();
+      });
+    }
+  };
+
   updatePiano = (event: MidiNoteType, note: number) => {
     const relativeNote = note - 21;
     const key = this.midi.activeNotes[note];
     const whiteIndex = this.whiteKeyMapping.indexOf(relativeNote % 12);
     if (whiteIndex !== -1) {
       const whiteKeyIndex = (Math.floor(relativeNote / 12) * 7) + whiteIndex;
+      let color = 'white'
+      if (event === MidiNoteType.noteon) {
+        color = key.color;
+
+        const notes = this.pianoRoll[relativeNote];
+        let subId = 0;
+        if (notes.length > 0) {
+          const previousNote = String(notes[notes.length - 1].key);
+          if (previousNote) {
+            subId = Number(previousNote.split('-')[1]) + 1;
+          }
+        }
+        this.pianoRoll[relativeNote].push(
+          <g key={`${relativeNote}-${subId}`} transform='scale(1,-1)'>
+            <rect
+              {...this.keyboard[whiteKeyIndex].props}
+              y='0'
+              width='1'
+              height='1'
+              fill={color}
+            >
+              <animate
+                id={`n${relativeNote}s${subId}`}
+                ref={this.heightAnimation(relativeNote)}
+                attributeName='height'
+                from='0'
+                to='43'
+                dur='5s'
+                fill='freeze'
+                begin='indefinite'
+              />
+              <animate
+                id={`n${note}-2`}
+                ref={this.positionAnimation(relativeNote)}
+                attributeName='y'
+                from='0'
+                to='43'
+                dur='5s'
+                fill='freeze'
+                begin={`n${relativeNote}s${subId}.end`}
+              />
+            </rect>
+          </g>
+        );
+      } else {
+        this.pianoRollActive[relativeNote].endElement();
+      }
       this.keyboard[whiteKeyIndex] = (
           <rect
             {...this.keyboard[whiteKeyIndex].props}
             key={relativeNote}
-            fill={event === MidiNoteType.noteon ? key.color : 'white'} />
+            fill={color} />
       );
     } else {
       const blackIndex = this.blackKeyMappingNoPadding.indexOf(relativeNote % 12);
       const blackKeyIndex = ((Math.floor(relativeNote / 12) * 5) + blackIndex);
+      let color = 'black'
+      if (event === MidiNoteType.noteon) {
+        color = key.color;
+
+        const notes = this.pianoRoll[relativeNote];
+        let subId = 0;
+        if (notes.length > 0) {
+          const previousNote = String(notes[notes.length - 1].key);
+          if (previousNote) {
+            subId = Number(previousNote.split('-')[1]) + 1;
+          }
+        }
+        this.pianoRoll[relativeNote].push(
+          <g key={`${relativeNote}-${subId}`} transform='scale(1,-1)'>
+            <rect
+              {...this.keyboard[52 + blackKeyIndex].props}
+              y='0'
+              width='1'
+              height='1'
+              fill={color}
+            >
+              <animate
+                id={`n${relativeNote}s${subId}`}
+                ref={this.heightAnimation(relativeNote)}
+                attributeName='height'
+                from='0'
+                to='43'
+                dur='5s'
+                fill='freeze'
+                begin='indefinite'
+              />
+              <animate
+                id={`n${note}-2`}
+                ref={this.positionAnimation(relativeNote)}
+                attributeName='y'
+                from='0'
+                to='43'
+                dur='5s'
+                fill='freeze'
+                begin={`n${relativeNote}s${subId}.end`}
+              />
+            </rect>
+          </g>
+        );
+      } else {
+        this.pianoRollActive[relativeNote].endElement();
+      }
       this.keyboard[52 + blackKeyIndex] = (
           <rect
             {...this.keyboard[52 + blackKeyIndex].props}
             key={relativeNote}
-            fill={event === MidiNoteType.noteon ? key.color : 'black'} />
+            fill={color} />
       );
     }
   }
@@ -187,8 +301,13 @@ class Piano extends React.PureComponent<IProps, IPianoState> {
     } = this.props;
     return (
       <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-        <svg viewBox="0 -42 52 52">
-          {this.keyboard}
+        <svg viewBox='0 -42 52 52'>
+          <g>
+            {this.keyboard}
+          </g>
+          <g>
+            {Array.from(this.pianoRoll.values()).flat()}
+          </g>
         </svg>
         <BrowserView>
           <button onClick={this.handleRecord}>{recording ? 'done' : 'record'}</button>
@@ -205,7 +324,7 @@ class Piano extends React.PureComponent<IProps, IPianoState> {
           <>
           <div>
             <select value={device} onChange={this.handleDeviceSelect}>
-              <option value=""></option>
+              <option value=''></option>
               {devices.map((device, i) => <option key={i} value={device}>{device}</option>)}
             </select>
             <select value={instrument} onChange={this.handleInstrumentSelect}>
